241d0c710eca3d8f02bf0e7c72f13b481a89b5ab
Livernier-Dev <chalakorn.khompot2003@gmail.com>
2023-12-02 02:15:05 +0700

    updata code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def check_server_status(ip, port):
 
 # ฟังก์ชันส่งข้อมูลไปยัง Discord Webhook
 def send_discord_webhook(message, mention_id=None):
-    webhook_url = "https://discord.com/api/webhooks/1180196031242182656/FXIsgPIE79WOYK3NeYQUkSyuz7t54eY93pGWrF19_-S5O-mso7yGRpk6CPcRF-K04gfw"
+    webhook_url = "webhook_url"
     payload = {
         "content": message,
         "allowed_mentions": {"users": [mention_id]} if mention_id else {}
@@ -25,11 +25,11 @@ def send_discord_webhook(message, mention_id=None):
 previous_status = {}
 
 # ตัวอย่างการใช้งาน
-server_ips = ["45.141.27.127", "102.129.138.139", "154.197.124.228"]
+server_ips = ["IP_1", "IP_2", "IP_3"]
 server_port = 80  # หรือ port ที่คุณต้องการตรวจสอบ
 
 # Discord user or role ID to mention (replace with the actual ID)
-mention_id = "<@737673774836482068>"
+mention_id = "ID_DISCORD"
 
 # เวลาที่รันครั้งแรก
 first_run_time = datetime.now()
@@ -37,32 +37,40 @@ print(f"First run time: {first_run_time}")
 send_discord_webhook(f"First run time: {first_run_time}")
 
 while True:
-    current_time = datetime.now()
-    status_changed = False  # Flag to check if any status has changed
+  current_time = datetime.now()
+  status_changed = False  # Flag to check if any status has changed
 
-    for ip in server_ips:
-        status = "online" if check_server_status(ip, server_port) else "offline"
-        send_discord_webhook(f"{current_time}:  Server Machine IP: {ip} {status}")
+  for ip in server_ips:
+    status = "online" if check_server_status(ip, server_port) else "offline"
+    send_discord_webhook(f"{current_time}:  Server Machine IP: {ip} {status}")
 
-        # ตรวจสอบการเปลี่ยนแปลง
-        if ip in previous_status and previous_status[ip] != status:
-            status_changed = True  # Set the flag to True
+    # ตรวจสอบการเปลี่ยนแปลง
+    if ip in previous_status and previous_status[ip] != status:
+      status_changed = True  # Set the flag to True
 
-        print("==========================================================================")
-        print(f"{current_time}: Server Machine IP: {ip} {status}")
-        print("==========================================================================")
+    print(
+        "=========================================================================="
+    )
+    print(f"{current_time}: Server Machine IP: {ip} {status}")
+    print(
+        "=========================================================================="
+    )
 
-        # ถ้าเครื่อง server offline ให้ส่งข้อมูลไปยัง Discord Webhook พร้อมแท็ก user หรือ role
-        if status == "offline":
-            send_discord_webhook(f"Server Machine IP: {ip} is offline.", mention_id)
+    # ถ้าเครื่อง server offline ให้ส่งข้อมูลไปยัง Discord Webhook พร้อมแท็ก user หรือ role
+    if status == "offline":
+      send_discord_webhook(f"Server Machine IP: {ip} is offline.",
+                           f"<@{mention_id}>")
 
-    # ตรวจสอบ Flag ว่ามีการเปลี่ยนแปลงหรือไม่
-    if status_changed:
-        # ส่งข้อมูลไปยัง Discord Webhook เมื่อมีการเปลี่ยนแปลง
-        send_discord_webhook(f"{current_time} : Server status changed.")
+  # ตรวจสอบ Flag ว่ามีการเปลี่ยนแปลงหรือไม่
+  if status_changed:
+    # ส่งข้อมูลไปยัง Discord Webhook เมื่อมีการเปลี่ยนแปลง
+    send_discord_webhook(f"{current_time} : Server status changed.")
 
-    # อัปเดตสถานะก่อนหน้า
-    previous_status = {ip: "online" if check_server_status(ip, server_port) else "offline" for ip in server_ips}
+  # อัปเดตสถานะก่อนหน้า
+  previous_status = {
+      ip: "online" if check_server_status(ip, server_port) else "offline"
+      for ip in server_ips
+  }
 
-    # หยุดทำงาน 1 นาที
-    time.sleep(60)
+  # หยุดทำงาน 5 นาที
+  time.sleep(300)
